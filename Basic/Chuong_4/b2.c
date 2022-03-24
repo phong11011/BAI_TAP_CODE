2fe504aaa3992ad5c970ab8ef8cf660c087cfdb4
phong11011 <92802073+phong11011@users.noreply.github.com>
2022-03-24 08:39:46 +0700

    Update b2.c

--- Basic/Chuong_4/b2.c
+++ Basic/Chuong_4/b2.c
@@ -1,17 +1,29 @@
-// #include <stdio.h>
+#include <stdio.h>
 
-// int main(int argc, char const *argv[])
-// {
-//     for (int i = 1; i <= 11; i++)
-//     {
-//         for (int j = 0; j < 6; j++)
-//         {
-//             if (j => 1 && j < 6)
-//             {
-//                 /* code */
-//             }
-               
-//         }   
-//     }
-//     return 0;
-// }
+int main(int argc, char const *argv[])
+{
+    int x = 11, y = 6;
+    int 
+    mid = (x - 1)/2;
+    for (int i = 0; i < y; ++i)
+    {
+        for (int j = 0; j < x; ++j)
+        {
+            // printf("i = %d | y = %d \n", i, y);
+            if (i == y - 1)
+            {
+                printf("*");
+                continue;
+            }
+            if (j <= (mid - i) || j >= (mid + i))
+            {
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
